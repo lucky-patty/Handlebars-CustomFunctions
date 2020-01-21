cdcdf1e1ebfb21a967815b7b72c4fb0d4561d8f7
lucky-patty <luck.wtj@gmail.com>
2020-01-21 00:31:45 -0800

    Add Loop function

---
+++ loop/loop.js
@@ -0,0 +1,11 @@
+/* For loop */
+
+const handlebars = require('handlebars');
+
+handlebars.registerHelper('for',(from,to,incr,block)=>{
+  var accum = '';
+  for(var i = from; i < to; i += incr){
+    accum += block.fn(i);
+  }
+  return accum;
+});
